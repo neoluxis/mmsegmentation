3b920a898ba4dcbf086b181e654e533de49b0524
Neolux Lee <neolux_lee@outlook.com>
2026-05-12 10:33:12 +0800

    segformer 训练流程更新

--- configs/swrd/segformer_mit-b0_4xb2-40k_swrd-512x512.py
+++ configs/swrd/segformer_mit-b0_4xb2-40k_swrd-512x512.py
@@ -1,8 +1,8 @@
 _base_ = [
-    '../_base_/models/segformer_mit-b0.py',
+    './segformer_mit-b0.py',
     '../_base_/datasets/swrd.py',
     '../_base_/default_runtime.py',
-    '../_base_/schedules/schedule_40k.py',
+    './schedule_80k.py',
 ]
 
 crop_size = (512, 512)
