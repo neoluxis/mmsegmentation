9e07840f2df6562e426b668d783706dcfedba574
Neolux Lee <neolux_lee@outlook.com>
2026-05-13 14:17:35 +0800

    feat: g更新了数据预处理和对比模型的运行配置

---
+++ configs/swrd/deeplabv3plus_r50-d8_4xb2-40k_swrd-512x512_gaussian.py
@@ -0,0 +1,30 @@
+_base_ = ['./deeplabv3plus_r50-d8_4xb2-40k_swrd-512x512.py']
+
+crop_size = (512, 512)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='GaussianDenoise', kernel_size=5, sigma=0.),
+    dict(type='LoadAnnotations'),
+    dict(
+        type='RandomResize',
+        scale=(1182, 817),
+        ratio_range=(0.5, 2.0),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.95),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='PackSegInputs')
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='GaussianDenoise', kernel_size=5, sigma=0.),
+    dict(type='Resize', scale=(1182, 817), keep_ratio=True),
+    dict(type='LoadAnnotations'),
+    dict(type='PackSegInputs')
+]
+
+train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
+val_dataloader = dict(dataset=dict(pipeline=test_pipeline))
+test_dataloader = val_dataloader
