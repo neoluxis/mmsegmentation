e633810a29e892c285aa736c56d279a6c4e4c978
Neolux Lee <neolux_lee@outlook.com>
2026-05-14 15:12:46 +0800

    fix b2 and b4

--- configs/swrd/segformer_mit-b4_8xb2-160k_swrd-512x512.py
+++ configs/swrd/segformer_mit-b4_8xb2-160k_swrd-512x512.py
@@ -1,6 +1,8 @@
 _base_ = [
-    '../segformer/segformer_mit-b4_8xb2-160k_ade20k-512x512.py',
-    '../_base_/datasets/swrd.py'
+    '../_base_/models/segformer_mit-b0.py',
+    '../_base_/datasets/swrd.py',
+    '../_base_/default_runtime.py',
+    '../_base_/schedules/schedule_160k.py'
 ]
 
 crop_size = (512, 512)
@@ -8,8 +10,12 @@ data_preprocessor = dict(size=crop_size)
 checkpoint = 'https://mmassets.onedl.ai/mmsegmentation/v0.5/pretrain/segformer/mit_b4_20220624-d588d980.pth'  # noqa
 model = dict(
     data_preprocessor=data_preprocessor,
-    backbone=dict(init_cfg=dict(type='Pretrained', checkpoint=checkpoint)),
-    decode_head=dict(num_classes=9),
+    backbone=dict(
+        init_cfg=dict(type='Pretrained', checkpoint=checkpoint),
+        embed_dims=64,
+        num_heads=[1, 2, 5, 8],
+        num_layers=[3, 8, 27, 3]),
+    decode_head=dict(in_channels=[64, 128, 320, 512], num_classes=9),
     test_cfg=dict(mode='slide', crop_size=crop_size, stride=(341, 341)))
 
 train_dataloader = dict(batch_size=1, num_workers=4)
