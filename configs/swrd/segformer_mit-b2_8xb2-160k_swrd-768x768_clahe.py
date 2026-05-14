fe057e027ca56dcf4835bc72abf2d00abc31c8a6
Neolux Lee <neolux_lee@outlook.com>
2026-05-15 00:02:01 +0800

    segformer more configs

---
+++ configs/swrd/segformer_mit-b2_8xb2-160k_swrd-768x768_clahe.py
@@ -0,0 +1,31 @@
+_base_ = ['./segformer_mit-b2_8xb2-160k_swrd-768x768.py']
+
+crop_size = (768, 768)
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='CLAHE', clip_limit=2.0, tile_grid_size=(8, 8)),
+    dict(type='LoadAnnotations'),
+    dict(
+        type='RandomResize',
+        scale=(1182, 817),
+        ratio_range=(0.5, 2.0),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.95),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Pad', size=crop_size),
+    dict(type='PackSegInputs')
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='CLAHE', clip_limit=2.0, tile_grid_size=(8, 8)),
+    dict(type='Resize', scale=(1182, 817), keep_ratio=True),
+    dict(type='LoadAnnotations'),
+    dict(type='PackSegInputs')
+]
+
+train_dataloader = dict(dataset=dict(pipeline=train_pipeline))
+val_dataloader = dict(dataset=dict(pipeline=test_pipeline))
+test_dataloader = val_dataloader
---
+++ configs/swrd/segformer_mit-b4_8xb2-160k_swrd-768x768.py
@@ -0,0 +1,39 @@
+_base_ = ['./segformer_mit-b4_8xb2-160k_swrd-512x512.py']
+
+crop_size = (768, 768)
+data_preprocessor = dict(size=crop_size)
+model = dict(
+    data_preprocessor=data_preprocessor,
+    test_cfg=dict(mode='slide', crop_size=crop_size, stride=(512, 512)))
+
+train_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='LoadAnnotations'),
+    dict(
+        type='RandomResize',
+        scale=(1182, 817),
+        ratio_range=(0.5, 2.0),
+        keep_ratio=True),
+    dict(type='RandomCrop', crop_size=crop_size, cat_max_ratio=0.95),
+    dict(type='RandomFlip', prob=0.5),
+    dict(type='PhotoMetricDistortion'),
+    dict(type='Pad', size=crop_size),
+    dict(type='PackSegInputs')
+]
+
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(type='Resize', scale=(1182, 817), keep_ratio=True),
+    dict(type='LoadAnnotations'),
+    dict(type='PackSegInputs')
+]
+
+train_dataloader = dict(
+    batch_size=2,
+    num_workers=4,
+    dataset=dict(pipeline=train_pipeline))
+val_dataloader = dict(
+    batch_size=1,
+    num_workers=4,
+    dataset=dict(pipeline=test_pipeline))
+test_dataloader = val_dataloader
