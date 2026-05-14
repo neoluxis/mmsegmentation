3ddb1d8033d9cefba297d6fee75284d05045dd38
Neolux Lee <neolux_lee@outlook.com>
2026-05-14 23:55:35 +0800

    segformer b2和b4 SGD改为 AdamW

--- configs/swrd/segformer_mit-b2_8xb2-160k_swrd-512x512.py
+++ configs/swrd/segformer_mit-b2_8xb2-160k_swrd-512x512.py
@@ -18,6 +18,35 @@ model = dict(
     decode_head=dict(in_channels=[64, 128, 320, 512], num_classes=9),
     test_cfg=dict(mode='slide', crop_size=crop_size, stride=(341, 341)))
 
+optim_wrapper = dict(
+    _delete_=True,
+    type='OptimWrapper',
+    optimizer=dict(
+        type='AdamW', lr=0.00006, betas=(0.9, 0.999), weight_decay=0.01),
+    paramwise_cfg=dict(
+        custom_keys={
+            'pos_block': dict(decay_mult=0.),
+            'norm': dict(decay_mult=0.),
+            'head': dict(lr_mult=10.)
+        }))
+
+param_scheduler = [
+    dict(
+        type='LinearLR',
+        start_factor=1e-6,
+        by_epoch=False,
+        begin=0,
+        end=1500),
+    dict(
+        type='PolyLR',
+        eta_min=0.0,
+        power=1.0,
+        begin=1500,
+        end=160000,
+        by_epoch=False,
+    )
+]
+
 train_dataloader = dict(batch_size=2, num_workers=4)
 val_dataloader = dict(batch_size=1, num_workers=4)
 test_dataloader = val_dataloader
